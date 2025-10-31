cebef60dd44b74ff4391ba623484f97fec35f99a
JoKhong <JoKhong@users.noreply.github.com>
2025-10-31 17:44:52 +0800

    Break down table to component

---
+++ StocksApp_RazorPages/ViewComponents/StockPriceCard.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedModels;
+
+namespace StocksApp_RazorPages.ViewComponents
+{
+    public class StockPriceCard : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Stock stock)
+        { 
+            return View(stock);
+        }
+    }
+}
