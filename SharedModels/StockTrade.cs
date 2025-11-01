3a97f10321d6545048c4dee43acf1e63e2277c18
JoKhong <JoKhong@users.noreply.github.com>
2025-11-01 14:23:15 +0800

    StockTrade model

---
+++ SharedModels/StockTrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels
+{
+    public class StockTrade
+    {
+        public string? StockSymbol { get; set; }
+        public string? StockName { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+    }
+}
---
+++ StocksApp_RazorPages/Controllers/TradeController.cs
@@ -0,0 +1,41 @@
+﻿using FinnhubContracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SharedModels;
+
+namespace StocksApp_RazorPages.Controllers
+{
+    public class TradeController : Controller
+    {
+        private readonly IFinnhubService _finnhubService;
+        private readonly IOptions<TradingOptions> _tradingOptions;
+
+        public TradeController(IFinnhubService finnhub, IOptions<TradingOptions> tradingOptions)
+        {
+            _finnhubService = finnhub;
+            _tradingOptions = tradingOptions;
+        }
+
+        [Route("trade")]
+        public async Task<IActionResult> Index()
+        {
+            if (_tradingOptions.Value.DefaultStockSymbol == null)
+            {
+                _tradingOptions.Value.DefaultStockSymbol = "MSFT";
+            }
+
+            Dictionary<string, object>? stockQuote = await _finnhubService.GetStockPriceQuote(_tradingOptions.Value.DefaultStockSymbol);
+            Dictionary<string, object>? companyProfile = await _finnhubService.GetCompanyProfile(_tradingOptions.Value.DefaultStockSymbol);
+
+            StockTrade stockTrade = new SharedModels.StockTrade
+            {
+                StockSymbol = Convert.ToString(companyProfile["ticker"].ToString()),
+                StockName = Convert.ToString(companyProfile["name"].ToString()),
+                Price = Convert.ToDouble(stockQuote["c"].ToString()),
+                Quantity = Convert.ToDouble(stockQuote["shareOutstanding"].ToString()),
+            };
+
+            return View(stockTrade);
+        }
+    }
+}
